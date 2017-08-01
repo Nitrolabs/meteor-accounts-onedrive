07083480ed7fa5c30f35902fc6da5bdd3ace328f
Max Ferguson <maxkferg@gmail.com>
2017-07-31 19:51:20 -0700

    v1.0.2

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "nitrolabs:accounts-onedrive",
   summary: "OneDrive OAuth flow",
-  version: "1.0.1",
+  version: "1.0.2",
   git: "https://github.com/nitrolabs/meteor-accounts-onedrive.git"
 });
 
